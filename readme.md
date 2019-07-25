f28a8fbe6699a3e1e97291d98bcf5e67032358aa
unknown <kevin.leptons@gmail.com>
2019-07-25 22:46:30 +0700

    add API seed.load()

--- readme.md
+++ readme.md
@@ -1,3 +1,51 @@
 # @trop/seed
 
-Define and load configuration file.
+Define, load and verify configuration file. It is simple but useful, save you from
+design configuration for application.
+
+## Tutorial
+
+```js
+const seed = require('@trop/seed')
+
+let schema = {
+    type: 'object',
+    properties: {
+        name: {
+            type: 'string'
+        },
+        age: {
+            type: 'integer',
+            minimum: 0
+        }
+    }
+}
+let conf = seed.load(schema, 'conf_file.yaml')
+```
+
+## APIs
+
+### seed.load(schema, file)
+
+* Input
+    * `schema` / `Object` - JSON Schema which describes configurations
+    * `file`/ `String` - Path to configuration file, written by YAML syntax
+* Process
+    * Load and parse configurations from file
+    * Verify configurations by schema
+* Output
+    * `Object` - Configurations
+* Exception
+    * `seed.InvalidConfigFile` - Configuration is invalid
+    * and other errors
+
+## Todo
+
+* `seed.load()` does not set default value for attributes because it is quite
+  complex. If any one has resources then let's research and make a pull
+  request, I am glad to merge it.
+
+## References
+
+* [Contribution](contribution.md)
+* [Changelog](changelog.md)
--- test/load.js
+++ test/load.js
@@ -1,7 +1,87 @@
+const path = require('path')
+const assert = require('assert')
+
 const seed = require('../lib')
 
 describe('seed.load()', () => {
-    it('with valid configuration', () => {
+    let schema = {
+        type: 'object',
+        additionalProperties: false,
+        required: [
+            'name',
+            'age',
+            'address'
+        ],
+        properties: {
+            name: {
+                type: 'string'
+            },
+            age: {
+                type: 'integer',
+                minimum: 0
+            },
+            address: {
+                type: 'string'
+            }
+        }
+    }
 
+    it('with not existed file', () => {
+        let file = _conf_file_path('not_existed_file.yaml')
+
+        assert.throws(() => {
+            seed.load(schema, file)
+        }, {
+            name: 'Error',
+            code: 'ENOENT'
+        })
+    })
+
+    it('with valid configuration file', () => {
+        let file = _conf_file_path('valid.yaml')
+        let conf = seed.load(schema, file)
+
+        assert.equal(conf.name, 'kevin')
+        assert.equal(conf.age, 18)
+        assert.equal(conf.address, 'Vietnam')
+    })
+
+    it('with empty configuration file', () => {
+        let file = _conf_file_path('empty.yaml')
+
+        assert.throws(() => {
+            seed.load(schema, file)
+        }, {
+            name: 'InvalidConfigFile',
+            message: file
+        })
+    })
+
+    it('with missing attribute from configuration file', () => {
+        let file = _conf_file_path('miss_attribute.yaml')
+
+        assert.throws(() => {
+            seed.load(schema, file)
+        }, {
+            name: 'InvalidConfigFile',
+            message: file
+        })
+    })
+
+    it('with invalid type attribute from configuration file', () => {
+        let file = _conf_file_path('invalid_type.yaml')
+
+        assert.throws(() => {
+            seed.load(schema, file)
+        }, {
+            name: 'InvalidConfigFile',
+            message: file
+        })
     })
 })
+
+// private members
+
+function _conf_file_path(relative_path) {
+    return path.join(__dirname, 'conf_file', relative_path)
+}
